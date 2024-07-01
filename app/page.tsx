2c36aa06ad79e46017c0f3af2a13e35278db2ad1
dice1546 <daniyal.afaqi@gmail.com>
2024-07-01 05:52:18 +0500

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
     <main className='flex h-full flex-col 
     items-center justify-center 
     bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] 
-    from-sky-800 to-slate-800'>
+    from-sky-900 to-slate-900'>
       <div className='space-y-6 text-center'>
         <h1 className={cn('text-6xl font-semibold text-white drop-shadow-md', font.className,)}>
          🔐 Auth
